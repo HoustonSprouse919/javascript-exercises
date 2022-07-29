e5099d852c5f3f5c2003764c20a9194e7b2f5920
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-07-29 16:34:49 -0400

    just getting started with removeFromArray

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, amount) {
+    let result = "";
+    if(amount < 0){
+        return "ERROR";
+    }
+    else{
+for(let i =0; i < amount; i++){
+    result += string;
+}
+return result;
+}
+}
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let text = "";
+    for(i =string.length; i > -1; i--){
+      let letter =  string.charAt(i);
+      text += letter;
+    }
+    return text;
 };
 
 // Do not edit below this line
