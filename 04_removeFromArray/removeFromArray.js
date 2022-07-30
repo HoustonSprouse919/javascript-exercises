023bf77c82beb118c8d7e230b099aaff459fbaae
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-07-29 22:48:57 -0400

    finished sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,20 @@
-const removeFromArray = function(array , remove ) {
-for(var i=0; i< array.length; i++){
-    if(array[i]=== remove){
-array.splice(i,1);
-    }
-}
-return array;
-};
+const removeFromArray = function (...args) {
+    // first we take out the array which is the first item
+    const array = args[0];
+    // create a new empty array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+      // push every element into the new array
+      // UNLESS it is included in the function arguments
+      // so we create a new array with every item, except those that should be removed
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    // and return that array
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let final =0;  //dumb dumb Houston kept "" around value which made it a string
+    let first =0;
+    let second = 0;
+    if(typeof num1 === "string" || typeof num2 === "string"){ //use typeof to check if both are strings to prevent us from returning them
+        final = "ERROR";
+    } else {
+    if (num1 > num2){  //all of these are just to get them in the right order to use in the for loop below
+        first = num2;
+        second = num1;
+    } else if (num1 < num2 ){
+        first = num1;
+        second = num2;
+    } else{
+        first = num1;
+        second = num2;
+    }
 
+    for(i = first; i < second + 1; i++){
+        final += i;
+    }
+}
+if (final > 0 ){
+    return final
+} else {
+    return("ERROR")
+} 
+
+    
 };
 
 // Do not edit below this line
